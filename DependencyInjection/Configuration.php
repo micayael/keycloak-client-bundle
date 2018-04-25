94f333ac503fb26209379f45964ee7eb88d4b9e5
Juan Ardissone <micayael@gmail.com>
2018-04-25 10:50:39 -0400

    Prueba

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,58 +18,58 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('authenticator_client');
+        $rootNode = $treeBuilder->root('keycloak_client');
 
-        $rootNode
-            ->children()
-                    ->scalarNode('host')
-                    ->info('Servidor del authenticator')
-                    ->cannotBeEmpty()
-                ->end()
-
-                ->scalarNode('token_uri')
-                    ->info('URI del servicio para obtener tokens')
-                    ->defaultValue('/api/jwt/token')
-                    ->cannotBeEmpty()
-                ->end()
-
-                ->enumNode('type')
-                    ->info('indica si la autenticación contra el servicio del Authenticator debe ser usando basic authentication o app_id')
-                    ->values(array('basic_auth', 'app_id'))
-                    ->isRequired()
-                ->end()
-
-                ->scalarNode('change_password_url')
-                    ->info('indica la url del authenticator para que el usuario solicite nuevamente su password')
-                    ->isRequired()
-                ->end()
-
-                ->arrayNode('basic_auth')
-                    ->cannotBeEmpty()
-                    ->children()
-                        ->scalarNode('username')
-                            ->info('Usuario para autenticación por basic authentication')
-                            ->cannotBeEmpty()
-                        ->end()
-                        ->scalarNode('password')
-                            ->info('Password para autenticación por basic authentication')
-                            ->cannotBeEmpty()
-                        ->end()
-                    ->end()
-                ->end()
-
-                ->scalarNode('app_id')
-                    ->info('AppId para conexión en caso de haber seleccionado esta forma de autenticación')
-                    ->cannotBeEmpty()
-                ->end()
-
-                ->scalarNode('default_target_route')
-                    ->info('Ruta predeterminada para ingresar después de un login si no existe referrer')
-                    ->defaultValue('admin')
-                    ->cannotBeEmpty()
-                ->end()
-            ->end()
-        ;
+//        $rootNode
+//            ->children()
+//                    ->scalarNode('host')
+//                    ->info('Servidor del authenticator')
+//                    ->cannotBeEmpty()
+//                ->end()
+//
+//                ->scalarNode('token_uri')
+//                    ->info('URI del servicio para obtener tokens')
+//                    ->defaultValue('/api/jwt/token')
+//                    ->cannotBeEmpty()
+//                ->end()
+//
+//                ->enumNode('type')
+//                    ->info('indica si la autenticación contra el servicio del Authenticator debe ser usando basic authentication o app_id')
+//                    ->values(array('basic_auth', 'app_id'))
+//                    ->isRequired()
+//                ->end()
+//
+//                ->scalarNode('change_password_url')
+//                    ->info('indica la url del authenticator para que el usuario solicite nuevamente su password')
+//                    ->isRequired()
+//                ->end()
+//
+//                ->arrayNode('basic_auth')
+//                    ->cannotBeEmpty()
+//                    ->children()
+//                        ->scalarNode('username')
+//                            ->info('Usuario para autenticación por basic authentication')
+//                            ->cannotBeEmpty()
+//                        ->end()
+//                        ->scalarNode('password')
+//                            ->info('Password para autenticación por basic authentication')
+//                            ->cannotBeEmpty()
+//                        ->end()
+//                    ->end()
+//                ->end()
+//
+//                ->scalarNode('app_id')
+//                    ->info('AppId para conexión en caso de haber seleccionado esta forma de autenticación')
+//                    ->cannotBeEmpty()
+//                ->end()
+//
+//                ->scalarNode('default_target_route')
+//                    ->info('Ruta predeterminada para ingresar después de un login si no existe referrer')
+//                    ->defaultValue('admin')
+//                    ->cannotBeEmpty()
+//                ->end()
+//            ->end()
+//        ;
 
         return $treeBuilder;
     }
